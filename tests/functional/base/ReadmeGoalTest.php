0bb03668f5367b2c77997e1960a03803d4453779
Andrii Vasyliev <sol@hiqdev.com>
2015-10-05 16:29:09 +0000

    fixed tests for new readme license section

--- tests/functional/base/ReadmeGoalTest.php
+++ tests/functional/base/ReadmeGoalTest.php
@@ -38,16 +38,14 @@ class ReadmeGoalTest extends \Codeception\TestCase\Test
         $this->tester->hidev('init the-vendor/new-test-package --norequire --year=2015 "--description=The project longer decription"');
         $this->tester->hidev('README.md');
         $this->tester->assertFileHas('README.md', [
-            trim("
-New Test Package
-----------------
-
-The project longer decription"),
+            "New Test Package\n----------------",
+            'The project longer decription',
             '## Installation',
             'The preferred way to install this project is through [composer](http://getcomposer.org/download/).',
             'php composer.phar create-project "the-vendor/new-test-package:*" directory2install',
             '## Licence',
-            '[No license](http://choosealicense.com/licenses/no-license)',
+            'This project is released under the terms of the No license',
+            'Read more [here](http://choosealicense.com/licenses/no-license).',
             'Copyright © 2015, The Vendor',
         ]);
     }
