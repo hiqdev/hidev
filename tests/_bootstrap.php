de5d98f787b7d566ac6182e48c073b95071680b2
Andrii Vasyliev <sol@hiqdev.com>
2015-12-02 20:00:24 +0200

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -16,7 +16,6 @@ define('YII_DEBUG', true);
 $_SERVER['SCRIPT_NAME']     = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 Yii::setAlias('@hidev', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 Yii::setAlias('@hidev/bin', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin');
 Yii::setAlias('@hidev/tests', __DIR__);
