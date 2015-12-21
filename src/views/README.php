56d93535d784aa4a46232b4f2891577bf59b0d9e
Andrii Vasyliev <sol@hiqdev.com>
2015-12-21 10:13:57 +0000

    CHANGED: moved README goal to `hidev-readme`

--- src/views/README.php
+++
@@ -1,9 +0,0 @@
-<?= $config->readme->renderH1($config->package->headline ?: $config->package->title) ?>
-<?php if ($config->package->headline) : ?>
-<?= $config->readme->renderBold($config->package->title) ?>
-<?php endif ?>
-<?= $config->readme->renderBadges() ?>
-<?php if ($config->package->description) : ?>
-<?= $config->readme->renderText($config->package->description) ?>
-<?php endif ?>
-<?= $config->readme->renderSections() ?>
--- src/views/readme/License.php
+++
@@ -1,4 +0,0 @@
-This project is released under the terms of the <?= $config->package->license ?> [license](LICENSE).
-Read more [here](<?= $config->license->url ?>).
-
-Copyright © <?= $config->package->years ?>, <?= $config->vendor->titleAndHomepage ?>
