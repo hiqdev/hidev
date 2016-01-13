325446eedbc8e42c69231f0bb3475126b64bb43f
Andrii Vasyliev <sol@hiqdev.com>
2016-01-13 16:34:41 +0000

    phpcsfixed

--- _bootstrap.php
+++ _bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Task runner, code generator and build tool for easier continuos integration
+ *
+ * @link      https://github.com/hiqdev/hidev
+ * @package   hidev
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ */
+
 foreach ([__DIR__ . '/vendor/autoload.php', __DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
     if (file_exists($file)) {
         define('HIDEV_COMPOSER_INSTALL', $file);
@@ -11,4 +20,4 @@ if (!defined('HIDEV_COMPOSER_INSTALL')) {
     fwrite(STDERR, 'You need to set up project dependencies with composer');
 }
 
-require(HIDEV_COMPOSER_INSTALL);
+require HIDEV_COMPOSER_INSTALL;
