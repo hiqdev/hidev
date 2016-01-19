7fd043d6d374acf974a75b1c1529e59198462937
Andrii Vasyliev <sol@hiqdev.com>
2016-01-19 12:13:11 +0000

    fixed start year to 2015 everywhere

--- tests/functional/base/InitGoalTest.php
+++ tests/functional/base/InitGoalTest.php
@@ -6,7 +6,7 @@
  * @link      https://github.com/hiqdev/hidev
  * @package   hidev
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
  */
 
 namespace base;
