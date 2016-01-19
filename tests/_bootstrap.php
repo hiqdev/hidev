95c5736280edaf12bda569f1787e266b4481487d
Andrii Vasyliev <sol@hiqdev.com>
2016-01-19 12:47:26 +0000

    simplified tests/_bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -11,11 +11,6 @@
 
 error_reporting(-1);
 
-define('YII_ENABLE_ERROR_HANDLER', false);
-define('YII_DEBUG', true);
-$_SERVER['SCRIPT_NAME']     = '/' . __DIR__;
-$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once __DIR__ . '/../vendor/autoload.php';
-Yii::setAlias('@hidev', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
-Yii::setAlias('@hidev/bin', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin');
-Yii::setAlias('@hidev/tests', __DIR__);
+
+Yii::setAlias('@hidev', dirname(__DIR__));
