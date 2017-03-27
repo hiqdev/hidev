e76360d035fb8276c68b8dabd7230d202db9f57e
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-27 16:50:11 +0300

    Added PHPUnit 6 compatibility

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,15 +1,29 @@
 <?php
 /**
- * Automation tool mixed with code generator for easier continuous development
+ * PHPUnit plugin for HiDev.
  *
- * @link      https://github.com/hiqdev/hidev
- * @package   hidev
+ * @link      https://github.com/hiqdev/hidev-phpunit
+ * @package   hidev-phpunit
  * @license   BSD-3-Clause
  * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
-error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);
 
 require __DIR__ . '/../src/config/bootstrap.php';
-
 require __DIR__ . '/../.hidev/vendor/autoload.php';
+
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
