c2c2574aefb0441c66c512499cbc3e4a7c7c40d1
Andrii Vasyliev <sol@hiqdev.com>
2017-04-26 10:58:33 +0000

    removed loading extra config madness from Application and simplified createControllerById

--- src/base/Application.php
+++ src/base/Application.php
@@ -32,140 +32,16 @@ class Application extends \yii\console\Application implements ViewContextInterfa
         parent::__construct($config);
     }
 
-    public function loadExtraConfig($path)
-    {
-        $this->setExtraConfig(static::readConfig($path));
-    }
-
-    public static function readConfig($path)
-    {
-        $path = Yii::getAlias($path);
-
-        return file_exists($path) ? require $path : [];
-    }
-
-    /**
-     * Load extra config files.
-     * @param string $vendor path to vendor dir
-     */
-    public function loadExtraVendor($vendor)
-    {
-        $this->setExtraEnv(static::readVendorConfig($vendor, 'dotenv'));
-        $this->setExtraConfig(static::readVendorConfig($vendor, 'hidev'));
-    }
-
-    public function readVendorConfig($vendor, $name)
-    {
-        return static::readConfig(ConfigPlugin::path($name, $vendor));
-    }
-
-    /**
-     * Sets extra environment variables.
-     * @param array $config
-     */
-    public function setExtraEnv($vars)
-    {
-        foreach ($vars as $key => $value) {
-            $_ENV[$key] = $value;
-        }
-    }
-
-    /**
-     * Implements extra configuration.
-     * @param array $config
-     */
-    public function setExtraConfig($config)
-    {
-        $this->_config = $config = ArrayHelper::merge($config, $this->_config);
-        $backup = $this->get('config')->getItems();
-        $this->clear('config');
-
-        foreach (['params', 'aliases', 'modules', 'components', 'container'] as $key) {
-            if (isset($config[$key])) {
-                $this->{'setExtra' . ucfirst($key)}($config[$key]);
-            }
-        }
-
-        $this->get('config')->mergeItems($backup);
-    }
-
-    /**
-     * Implements extra params.
-     * @param array $params
-     */
-    public function setExtraParams($params)
-    {
-        if (is_array($params) && !empty($params)) {
-            $this->params = ArrayHelper::merge($this->params, $params);
-        }
-    }
-
-    /**
-     * Implements extra aliases.
-     * @param array $aliases
-     */
-    public function setExtraAliases($aliases)
-    {
-        if (is_array($aliases) && !empty($aliases)) {
-            $this->setAliases($aliases);
-        }
-    }
-
-    /**
-     * Implements extra modules.
-     * @param array $modules
-     */
-    public function setExtraModules($modules)
-    {
-        if (is_array($modules) && !empty($modules)) {
-            $this->setModules($modules);
-        }
-    }
-
-    /**
-     * Implements extra components.
-     * Does NOT touch already instantiated components.
-     * @param array $components
-     */
-    public function setExtraComponents($components)
-    {
-        if (is_array($components) && !empty($components)) {
-            foreach ($components as $id => $component) {
-                if ($this->has($id, true)) {
-                    unset($components[$id]);
-                }
-            }
-            $this->setComponents($components);
-        }
-    }
-
-    /**
-     * Implements extra container config.
-     * @param array $container config
-     */
-    public function setExtraContainer($container)
-    {
-        $this->setContainer($container);
-    }
+    protected $controllers = [];
 
     public function createControllerByID($id)
     {
-        /*
-        if ($this->_first) {
-            $this->_first = false;
-            static $skips = ['init' => 1, 'clone' => 1, '--version' => 1];
-            if (!isset($skips[$id])) {
-                $this->runRequest('start');
-            }
-        }
-        */
-
-        if ($this->get('config')->hasGoal($id)) {
-            return $this->get('config')->get($id);
+        if (isset($this->controllers[$id])) {
+            return $this->controllers[$id];
         }
 
         $controller = parent::createControllerByID($id);
-        $this->get('config')->set($id, $controller);
+        $this->controllers[$id] = $controller;
 
         return $controller;
     }
