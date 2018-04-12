e149f73f8c7ea64bc7399e64967ac30c6026879d
Andrii Vasyliev <sol@hiqdev.com>
2018-04-12 17:49:53 +0300

    added `Hidev` helper

---
+++ src/helpers/Hidev.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Automation tool mixed with code generator for easier continuous development
+ *
+ * @link      https://github.com/hiqdev/hidev
+ * @package   hidev
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hidev\helpers;
+
+use Yii;
+
+/**
+ * Hidev helper.
+ */
+class Hidev
+{
+    public static function exec($route, array $args, $wait = false)
+    {
+        $path = dirname(__DIR__) . '/bin/hidev';
+        $command = "$path $route";
+        foreach ($args as $arg) {
+            $command .= ' ' . escapeshellarg($arg);
+        }
+        $amp = $wait ? '' : '&';
+
+        return exec("$command > /dev/null 2>&1 $amp");
+    }
+}
