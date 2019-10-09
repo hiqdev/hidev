22a57d82fa989cb90b86b8b2f4ae48d85146d530
Andrii Vasyliev <sol@hiqdev.com>
2019-10-09 08:09:23 +0000

    Added `deploy` controller

---
+++ src/console/DeployController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Automation tool mixed with code generator for easier continuous development
+ *
+ * @link      https://github.com/hiqdev/hidev
+ * @package   hidev
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hidev\console;
+
+use hidev\components\Deployer;
+
+/**
+ * Deploy Controller.
+ * TODO REDO the whole thing to be configurable
+ */
+class DeployController extends \yii\console\Controller
+{
+    public $defaultAction = 'all';
+
+    public function actionAll($name = null, $command = 'all')
+    {
+        $deployer = new Deployer($name);
+
+        return $deployer->call($command);
+    }
+}
