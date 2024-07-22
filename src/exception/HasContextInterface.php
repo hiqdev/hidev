cb749b4cee69acfe9f9f706983bb9ddf116f9cb4
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-07-22 22:35:40 +0300

    HP-1900: Monitor changes in production envs

---
+++ src/exception/HasContextInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace hidev\exception;
+
+interface HasContextInterface extends \Throwable
+{
+    public function addContext(array $data): self;
+
+    public function getContext(): array;
+}
